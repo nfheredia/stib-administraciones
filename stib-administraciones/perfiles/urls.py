d23c682e7c0e38e53b9d3490167f0d1eaaa6332a
btenaglia <brunomartintenaglia@gmail.com>
2015-03-17 23:07:18 -0300

    Mostrar msg para editar perfil.

--- stib-administraciones/perfiles/urls.py
+++ stib-administraciones/perfiles/urls.py
@@ -2,10 +2,15 @@ from django.conf.urls import patterns, url
 
 from .views import PerfilesUpdateView
 
-urlpatterns = patterns('',
+urlpatterns = patterns('stib-administraciones.perfiles.views',
     url(
         regex=r'^update/$',
         view=PerfilesUpdateView.as_view(),
         name='perfiles_update'
+    ),
+    url(
+        r'^no-msg-bienvenida/$',
+        'no_mostrar_msg_bienvenida',
+        name='no_mostrar_msg_bienvenida'
     ),
 )
--- stib-administraciones/perfiles/views.py
+++ stib-administraciones/perfiles/views.py
@@ -2,6 +2,7 @@
 
 from django.views.generic import UpdateView
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 from braces.views import LoginRequiredMixin
 
@@ -22,3 +23,24 @@ class PerfilesUpdateView(LoginRequiredMixin, UpdateView):
     def get_success_url(self):
         messages.success(self.request, u'Tu perfil fué editado correctamente.')
         return self.success_url
+
+    def form_valid(self, form):
+        """
+        Cuando se actualiza el perfil,
+        no se debe mostrar mas el msg
+        de bienvenida para editar el perfil
+        """
+        form.instance.alerta_bienvenida = 0
+        return super(PerfilesUpdateView, self).form_valid(form)
+
+
+def no_mostrar_msg_bienvenida(request):
+    """
+    Marcar en 0 el campo para no mostrar mas
+    el msg de bienvenida en el perfil
+    del usuario logueado...
+    """
+    perfil = Perfiles.objects.get(user=request.user.id)
+    perfil.alerta_bienvenida = 0
+    perfil.save()
+    return HttpResponseRedirect('/')
