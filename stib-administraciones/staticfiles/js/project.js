3c4cbbe4a75307cdaa3abd8800de963b42aaf1ec
btenaglia <brunomartintenaglia@gmail.com>
2015-04-11 18:55:56 -0300

    Continuacion de fotos.

--- stib-administraciones/staticfiles/js/project.js
+++ stib-administraciones/staticfiles/js/project.js
@@ -2,4 +2,13 @@ $(function() {
 
     $('#side-menu').metisMenu();
 
+    $(".fancybox").fancybox({
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+
+	$("#todas-las-fotos").click(function(){
+        $("a.no-display")[0].click();
+	});
+
 });
