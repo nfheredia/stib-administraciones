3ec30f7470a943e2119d6e7164c0c380bbbb0433
btenaglia <brunomartintenaglia@gmail.com>
2015-04-11 11:18:34 -0300

    Comenzando a trabajar con fotos

---
+++ stib-administraciones/fotos/templatetags/fotos_tags.py
@@ -0,0 +1,7 @@
+from django import template
+from ..models import Fotos
+register = template.Library()
+
+@register.assignment_tag
+def obtener_fotos_por_edificio(edificio_id):
+    return Fotos.objects.filter(edificio=edificio_id).all()
